ad74c7cbc962f9a0c2e29b6ba1aac6f6dad20e5a
Balu <balu.gulla@gmail.com>
2015-08-22 18:04:57 -0400

    Added Thread programming.

--- gulla-sample-test/src/main/java/org/gulla/service/gulla/test/HelloWorld.java
+++ gulla-sample-test/src/main/java/org/gulla/service/gulla/test/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello world guys");
-
+        ThreadRunnable runnable = new ThreadRunnable();
+        Thread t = new Thread(runnable);
+        t.start();
 	}
 
 }
---
+++ gulla-sample-test/src/main/java/org/gulla/service/gulla/test/ThreadRunnable.java
@@ -0,0 +1,11 @@
+package org.gulla.service.gulla.test;
+
+public class ThreadRunnable implements Runnable  {
+
+	public void run() {
+
+      System.out.println("this is testing of Runnable interface");
+		
+	}
+
+}
